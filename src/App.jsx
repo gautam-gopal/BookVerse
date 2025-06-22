1878ce24dc3bb4d70e1a91bb8545f0f6a66a7e7d
Gautam Gopal <gautamgopal9847@gmail.com>
2025-06-22 15:13:14 +0530

    changed directory name from online-bookstore to bookVerse and created components and pages sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
+import { Routes, Route } from "react-router-dom"
+import Home from "./Pages/Home"
+import Cart from "./Pages/Cart"
+import Checkout from "./Pages/Checkout"
+import About from "./Pages/About"
+import NotFound from "./Pages/NotFound"
+import NotFound from "./Pages/NotFound"
+import Header from "./Components/Header"
+import Footer from "./Components/Footer"
+
 
 function App() {
   
   return (
-    <>
-      
-    </>
+    <div className="flex flex-col min-h-screen">
+      <Header/ >
+      <main className="flex-grow container mx-auto p-4">
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/book/:id" element={<BookDetails/>}/>
+          <Route path="/cart" element={<Cart/>}/>
+          <Route path="/checkout" element={<Checkout/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="*" element={<NotFound/>}/>
+        </Routes>
+      </main>
+      <Footer/>
+    </div>
   )
 }
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <h1><img src="./assets/bookstore-logo.jpg" alt="logo" />Online BookStore</h1>
+    <header>Header</header>
   )
 }
 
